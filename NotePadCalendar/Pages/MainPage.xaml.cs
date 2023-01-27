a173d111c0e25cd8ab8751400fcf33b402770d7e
FLAS SON <dinar-shigapov@inbox.ru>
2023-01-27 12:20:29 +0300

    Добавьте файлы проекта.

---
+++ NotePadCalendar/Pages/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using NotePadCalendar.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotePadCalendar.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LvReminds.ItemsSource = App.DB.Note.Where(x => x.UserId == App.LoggedUser.Id).ToList();
+        }
+        
+
+        private void AddBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            string errorMessage = "";
+
+            if (DatDp.SelectedDate == null)
+            {
+                errorMessage += "Выберите дату\n";
+            }
+            if (string.IsNullOrWhiteSpace(TbDescription.Text))
+            {
+                errorMessage += "Введите описание\n";
+            }
+            if (string.IsNullOrWhiteSpace(errorMessage) == false)
+            {
+                MessageBox.Show(errorMessage);
+                return;
+            }
+            App.DB.Note.Add(new Note()
+            {
+                NoteDate = DatDp.Text,
+                Description = TbDescription.Text,
+                UserId = App.LoggedUser.Id,
+
+            });
+            App.DB.SaveChanges();
+            Refresh();
+        }
+        public void Refresh()
+        {
+            LvReminds.ItemsSource = App.DB.Note.Where(x => x.UserId == App.LoggedUser.Id).ToList();
+        }
+        private void ExtBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            App.LoggedUser = null;
+            NavigationService.Navigate(new LoginPage());
+        }
+    }
+}
---
+++ NotePadCalendar/Pages/RegPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using NotePadCalendar.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotePadCalendar.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegPage.xaml
+    /// </summary>
+    public partial class RegPage : Page
+    {
+        public RegPage()
+        {
+            InitializeComponent();
+        }
+        private void RegCloseBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (LogTb.Text.Trim().Length <= 0 || PassPb.Password.Trim().Length <= 0 || RepPassPb.Password.Trim().Length <= 0)
+            {
+                MessageBox.Show("Заполните все поля!");
+            }
+            else
+            {
+                if (PassPb.Password.Trim().Length != RepPassPb.Password.Trim().Length)
+                {
+                    MessageBox.Show("Пароли не совпадают!");
+                }
+                else
+                {
+                    App.LoggedUser = App.DB.User.ToList().Find(x => x.Login == LogTb.Text);
+                    if (App.LoggedUser != null)
+                    {
+                        MessageBox.Show("Такой логин уже есть!");
+                    }
+                    else
+                    {
+                        App.DB.User.Add(new User()
+                        {
+                            Login = LogTb.Text.Trim(),
+                            Password = PassPb.Password.Trim(),
+                        });
+                        App.DB.SaveChanges();
+                        NavigationService.Navigate(new LoginPage());
+                    }
+
+                }
+            }
+        }
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
